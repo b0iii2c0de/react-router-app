281562be2e88a28ebae8e6a2c4a8b74ead48aac6
c0de b0iii <boiiicode@gmail.com>
2023-06-20 22:13:57 +0300

    feat: start router-app task, configure prod and made overall adjusments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
+import NewPost from './components/newpost/NewPost';
+import PostPage from './components/postpage/PostPage';
+import About from './components/about/About';
+import Missing from './components/missing/Missing';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} /> 
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
